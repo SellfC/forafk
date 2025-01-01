f50d37a0ba96140cea0cb8fdcb4a863a1ddf8193
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-01 19:09:39 +0300

    Update and rename aarasp.lua to aamaria.lua

---
+++ aamaria.lua
@@ -0,0 +1,9 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+task.wait(5)
+setfpscap(15)
+script_key="sJcLrXZmMudnvcTXLQGlVJXcrxOgQUqo";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e32274866a41a919678395d899e10bff.lua"))()
+task.wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/betterbuffer.lua"))()
+task.wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/confbettercard.lua"))()
