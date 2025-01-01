9769a0dd0838c531db599f9db07694d49d5b0ebb
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-01 19:20:35 +0300

    Create raspr.lua

---
+++ raspr.lua
@@ -0,0 +1,8 @@
+local Player = game:GetService("Players").LocalPlayer
+if Player.Name == "MariaRobinson9" then
+print("load scripts")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/aamaria.lua"))()
+else
+print("not maria")
+  break
+end
