643e808906dc4df2a59774f3945587e4aad61ee2
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-03 17:46:47 +0300

    Update aamaria.lua

--- aamaria.lua
+++ aamaria.lua
@@ -1,6 +1,5 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
 task.wait(5)
-setfpscap(15)
 script_key="sJcLrXZmMudnvcTXLQGlVJXcrxOgQUqo";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e32274866a41a919678395d899e10bff.lua"))()
 task.wait(1)
