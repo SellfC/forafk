90fd90bff35543c82ea763255b3368e2cd6f2a28
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-09 16:21:41 +0300

    Create halal.lua

---
+++ halal.lua
@@ -0,0 +1,22 @@
+setfpscap(8)
+getgenv().RequiredLevel = 25
+
+getgenv().HolidayHuntFarm = true
+getgenv().AutoBuyIcyStar = true
+
+getgenv().NightmareHuntFarm = false
+getgenv().AutoBuySpookyStar = true
+
+getgenv().AutoEquipBoostUnits = true
+getgenv().AutoOpenCapsule = true
+
+getgenv().AutoSellRareSkins = true
+getgenv().AutoSellEpicSkins = true
+getgenv().AutoSellLegendarySkins = true
+
+getgenv().WebhookURL = "https://discord.com/api/webhooks/1290727516256075827/RYR2_LHEmk-5fbii_cOuq_hfZXCWw5w1SQ7lEw1DbomFS91xjq2F5-ApzFE84h1U_961"
+getgenv().BoostFPS = true
+getgenv().Disable3DRender = true
+
+script_key="NeYlQuZmRJZmHNdxRdnBuiNiWOWqCqib";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c8c006e970ed3323cef563220f3e3984.lua"))()
