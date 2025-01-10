7afed3aacdcecd02c9408c5489b16fa9d2cba392
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-10 20:28:54 +0300

    Update raspr.lua

--- raspr.lua
+++ raspr.lua
@@ -1,7 +1,9 @@
 local Player = game:GetService("Players").LocalPlayer
 if Player.Name == "MariaRobinson9" then
 print("load scripts")
+setfpscap(15)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/aamaria.lua"))()
 else
+setfpscap(8)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/halal.lua"))()
 end
