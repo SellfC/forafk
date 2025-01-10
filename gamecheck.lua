e64582ee710daf832b89663dc32a08ff418b84da
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-10 20:28:18 +0300

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 if game.CreatorId == 10611639 then
 print("load scripts")
-loadstring(game:HttpGet("https://github.com/SellfC/forafk/blob/main/raspr.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/raspr.lua"))()
 else
 print(game.CreatorId)
 end
