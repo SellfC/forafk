f5ab86fd0c4bc3657ae88ff86902e4b7fb358176
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-12 14:17:47 +0300

    Update aamaria.lua

--- aamaria.lua
+++ aamaria.lua
@@ -1,10 +1,9 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
-task.wait(5)
+task.wait(10)
 script_key="sJcLrXZmMudnvcTXLQGlVJXcrxOgQUqo";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e32274866a41a919678395d899e10bff.lua"))()
-task.wait(1)
+task.wait( math.random() )
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/betterbuffer.lua"))()
-task.wait(1)
+task.wait( math.random() )
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/confbettercard.lua"))()
-task.wait(1)
 print("aamaria.lua loaded successfully")
