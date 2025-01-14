0b0a4e89a13b39a7ea63dcf1f97384dea05c89e6
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-14 19:21:52 +0300

    Update nousigi.lua

--- nousigi.lua
+++ nousigi.lua
@@ -1,39 +1,41 @@
 getgenv().Config = {
-	["AutoSave"] = true,
 	["Summoner"] = {
 		["Exchange Legacy Gem"] = 0
 	},
+	["Game Finished"] = {
+		["Auto Replay"] = true
+	},
+	["Joiner Cooldown"] = 0,
 	["Performance"] = {
 		["Delete Map"] = true,
 		["Boost FPS"] = true,
 		["Black Screen"] = true
 	},
 	["Webhook"] = {
-		["Game Finished"] = false,
 		["URL"] = "https://discord.com/api/webhooks/1328743860427554826/QjCDnSrAO2zS7eQY1YwoFcSpzAjJWSQMKp5bx5VU8z31nNqWmXYHf76mtybIJBFITc1H"
 	},
-	["Secure"] = {
-		["Walk Around"] = false
-	},
 	["Macros"] = {
+		["No Ignore Sell Timing"] = true,
 		["Macro Retry Limit"] = 0,
-		["Ignore Macro Timing"] = true,
-		["No Ignore Sell Timing"] = true
+		["Ignore Macro Timing"] = true
 	},
+	["AutoSave"] = true,
 	["Gameplay"] = {
-		["Auto Sell"] = {
-			["Wave"] = 1
-		},
+		["Modded Placement"] = true,
 		["Smart Auto Ability"] = {
 			["Infinite buff rotation delay"] = 15.9
 		},
 		["Auto Leave"] = {
 			["Wave"] = 1
 		},
+		["Auto Vote Start"] = true,
+		["Auto Skip Wave"] = true,
 		["Auto Sell Farm"] = {
 			["Wave"] = 1
+		},
+		["Auto Sell"] = {
+			["Wave"] = 1
 		}
-	},
-	["Joiner Cooldown"] = 0
+	}
 }
 repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(20)until Joebiden
