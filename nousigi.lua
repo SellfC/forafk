45fae9ecad8cedf77cdc8772825f5c2fb1d36080
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-15 18:55:12 +0300

    Update nousigi.lua

--- nousigi.lua
+++ nousigi.lua
@@ -1,30 +1,199 @@
 getgenv().Config = {
+	["Inventory"] = {
+		["Auto Open Star"] = {
+			["Icy Star"] = true
+		}
+	},
 	["Summoner"] = {
 		["Exchange Legacy Gem"] = 0
 	},
 	["Game Finished"] = {
 		["Auto Replay"] = true
 	},
-	["Joiner Cooldown"] = 0,
 	["Performance"] = {
 		["Delete Map"] = true,
-		["Boost FPS"] = true,
-		["Black Screen"] = true
+		["Boost FPS"] = true
 	},
-	["Webhook"] = {
-		["URL"] = "https://discord.com/api/webhooks/1328743860427554826/QjCDnSrAO2zS7eQY1YwoFcSpzAjJWSQMKp5bx5VU8z31nNqWmXYHf76mtybIJBFITc1H"
+	["Joiner Cooldown"] = 0,
+	["Auto Play"] = {
+		["Auto Upgrade"] = true,
+		["Enable"] = true,
+		["Auto Fuse Vego-Carrot Anchor"] = "Carrot",
+		["Placement Type"] = {
+			["Otherwordly Plane"] = "Middle Position",
+			["Frozen Abyss"] = "Middle Position",
+			["Strange Town"] = "Middle Position",
+			["Fiend City"] = "Middle Position",
+			["Magic Hills"] = "Middle Position",
+			["Sky Aircraft"] = "Middle Position",
+			["Snowy Town"] = "Middle Position",
+			["Alien Spaceship (Underwater)"] = "Middle Position",
+			["Fabled Kingdom"] = "Middle Position",
+			["Puppet Island (Summer)"] = "Middle Position",
+			["Nightmare Train"] = "Middle Position",
+			["Alien Spaceship (Final)"] = "Middle Position",
+			["Strange Town (Haunted)"] = "Middle Position",
+			["Ruined City (The Menace)"] = "Middle Position",
+			["Haunted Academy"] = "Middle Position",
+			["Anniversary Island"] = "Middle Position",
+			["Fabled Kingdom (Cube)"] = "Middle Position",
+			["Puppet Island (Threads)"] = "Middle Position",
+			["Magic Town"] = "Middle Position",
+			["Haunted Academy (Summer)"] = "Middle Position",
+			["Alien Spaceship"] = "Middle Position",
+			["Storm Hideout"] = "Middle Position",
+			["Walled City"] = "Middle Position",
+			["Planet Greenie (Haunted)"] = "Middle Position",
+			["Navy Bay"] = "Middle Position",
+			["Snowy Kingdom"] = "Middle Position",
+			["Virtual Dungeon (Bosses)"] = "Middle Position",
+			["Magic Hills (Demonic)"] = "Middle Position",
+			["Ant Kingdom (Summer)"] = "Middle Position",
+			["Rain Village"] = "Middle Position",
+			["Fabled Kingdom (Summer)"] = "Middle Position",
+			["Wish City"] = "Middle Position",
+			["Spirit World"] = "Middle Position",
+			["Magic Town (Snow)"] = "Middle Position",
+			["Sacred Planet"] = "Middle Position",
+			["Walled City (Midnight)"] = "Middle Position",
+			["Space Center (New Moon)"] = "Middle Position",
+			["Storm Hideout (Final)"] = "Middle Position",
+			["Planet Greenie (Summer)"] = "Middle Position",
+			["Haunted Academy (Frozen)"] = "Middle Position",
+			["Walled City (Summer)"] = "Middle Position",
+			["Sand Village (Snow)"] = "Middle Position",
+			["Fabled Kingdom (Generals)"] = "Middle Position",
+			["Spirit Invasion"] = "Middle Position",
+			["Future City (Tyrant's Invasion)"] = "Middle Position",
+			["Hellish City"] = "Middle Position",
+			["Virtual Dungeon"] = "Middle Position",
+			["Sand Village"] = "Middle Position",
+			["Space Center"] = "Middle Position",
+			["Future City"] = "Middle Position",
+			["Planet Greenie (Frozen)"] = "Middle Position",
+			["Puppet Island"] = "Middle Position",
+			["Magic Hills (Elf Invasion)"] = "Middle Position",
+			["Dungeon Throne"] = "Middle Position",
+			["Ruined City"] = "Middle Position",
+			["Navy Bay (Midnight)"] = "Middle Position",
+			["Fiend City (Winter)"] = "Middle Position",
+			["Cursed Festival"] = "Middle Position",
+			["Frozen Abyss (Six Realms)"] = "Middle Position",
+			["Power Contest"] = "Middle Position",
+			["Haunted Mansion"] = "Middle Position",
+			["Magic Town (Haunted)"] = "Middle Position",
+			["Ruined City (Midnight)"] = "Middle Position",
+			["Ant Kingdom"] = "Middle Position",
+			["Mountain Temple"] = "Middle Position",
+			["Walled City (Winter)"] = "Middle Position",
+			["Spirit Town"] = "Middle Position",
+			["Planet Greenie"] = "Middle Position"
+		},
+		["Upgrade Method"] = "Lowest Level (Spread Upgrade)",
+		["Distance Threshold"] = {
+			["Otherwordly Plane"] = 15,
+			["Frozen Abyss"] = 15,
+			["Strange Town"] = 15,
+			["Fiend City"] = 15,
+			["Magic Hills"] = 15,
+			["Sky Aircraft"] = 15,
+			["Snowy Town"] = 15,
+			["Alien Spaceship (Underwater)"] = 15,
+			["Fabled Kingdom"] = 15,
+			["Puppet Island (Summer)"] = 15,
+			["Nightmare Train"] = 15,
+			["Alien Spaceship (Final)"] = 15,
+			["Strange Town (Haunted)"] = 15,
+			["Ruined City (The Menace)"] = 15,
+			["Haunted Academy"] = 15,
+			["Anniversary Island"] = 15,
+			["Fabled Kingdom (Cube)"] = 15,
+			["Puppet Island (Threads)"] = 15,
+			["Magic Town"] = 15,
+			["Haunted Academy (Summer)"] = 15,
+			["Alien Spaceship"] = 15,
+			["Storm Hideout"] = 15,
+			["Walled City"] = 15,
+			["Planet Greenie (Haunted)"] = 15,
+			["Navy Bay"] = 15,
+			["Snowy Kingdom"] = 15,
+			["Virtual Dungeon (Bosses)"] = 15,
+			["Magic Hills (Demonic)"] = 15,
+			["Ant Kingdom (Summer)"] = 15,
+			["Rain Village"] = 15,
+			["Fabled Kingdom (Summer)"] = 15,
+			["Wish City"] = 15,
+			["Spirit World"] = 15,
+			["Magic Town (Snow)"] = 15,
+			["Sacred Planet"] = 15,
+			["Walled City (Midnight)"] = 15,
+			["Space Center (New Moon)"] = 15,
+			["Storm Hideout (Final)"] = 15,
+			["Planet Greenie (Summer)"] = 15,
+			["Haunted Academy (Frozen)"] = 15,
+			["Walled City (Summer)"] = 15,
+			["Sand Village (Snow)"] = 15,
+			["Fabled Kingdom (Generals)"] = 15,
+			["Spirit Invasion"] = 15,
+			["Future City (Tyrant's Invasion)"] = 15,
+			["Hellish City"] = 15,
+			["Virtual Dungeon"] = 15,
+			["Sand Village"] = 15,
+			["Space Center"] = 15,
+			["Future City"] = 15,
+			["Planet Greenie (Frozen)"] = 15,
+			["Puppet Island"] = 15,
+			["Magic Hills (Elf Invasion)"] = 15,
+			["Dungeon Throne"] = 15,
+			["Ruined City"] = 15,
+			["Navy Bay (Midnight)"] = 15,
+			["Fiend City (Winter)"] = 15,
+			["Cursed Festival"] = 15,
+			["Frozen Abyss (Six Realms)"] = 15,
+			["Power Contest"] = 15,
+			["Haunted Mansion"] = 15,
+			["Magic Town (Haunted)"] = 15,
+			["Ruined City (Midnight)"] = 15,
+			["Ant Kingdom"] = 15,
+			["Mountain Temple"] = 15,
+			["Walled City (Winter)"] = 15,
+			["Spirit Town"] = 15,
+			["Planet Greenie"] = 15
+		},
+		["Upgrade Cap"] = {
+			["1"] = 0,
+			["3"] = 0,
+			["2"] = 0,
+			["5"] = 0,
+			["4"] = 0,
+			["6"] = 0
+		},
+		["Middle Position"] = {
+			["Planet Greenie"] = "-2947.08154296875, 91.80620574951172, -704.0753173828125"
+		}
+	},
+	["Claimer"] = {
+		["Auto Claim Level Milestone"] = true,
+		["Auto Claim Quest"] = true,
+		["Auto Accept Daily Mission"] = true,
+		["Auto Claim Present"] = true
 	},
 	["Macros"] = {
 		["No Ignore Sell Timing"] = true,
 		["Macro Retry Limit"] = 0,
 		["Ignore Macro Timing"] = true
 	},
+	["Secure"] = {
+		["Walk Around"] = true
+	},
 	["AutoSave"] = true,
+	["World Joiner"] = {
+		["Act"] = "Act 1",
+		["Enable"] = true,
+		["World"] = "Planet Greenie"
+	},
 	["Gameplay"] = {
 		["Modded Placement"] = true,
-		["Smart Auto Ability"] = {
-			["Infinite buff rotation delay"] = 15.9
-		},
 		["Auto Leave"] = {
 			["Wave"] = 1
 		},
@@ -36,6 +205,10 @@ getgenv().Config = {
 		["Auto Sell"] = {
 			["Wave"] = 1
 		}
+	},
+	["Failsafe"] = {
+		["Teleport Lobby if Player"] = true
 	}
 }
+getgenv().Key = "k8f732e0f88c9d260d9ae406"
 repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(20)until Joebiden
