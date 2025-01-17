4734d84a6341c53487456e3d3a4bd35ac5b460f7
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-17 22:08:51 +0300

    Update raspr.lua

--- raspr.lua
+++ raspr.lua
@@ -11,5 +11,5 @@ if id[PlayerId] then
     loadstring(game:HttpGet(id[PlayerId]))()
 else
     setfpscap(15)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/bf.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/nousigi.lua"))()
 end
