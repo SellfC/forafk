59531a5452bf97c36e5cd00a77188e2591cf46f9
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-24 21:08:50 +0300

    Update bf.lua

--- bf.lua
+++ bf.lua
@@ -1,39 +1,24 @@
-
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local request_join_lobby = ReplicatedStorage:WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("request_join_lobby")
+local user = "MariaRobinson9"
 local lobbies = workspace:WaitForChild("_LOBBIES"):WaitForChild("Story")
-
-while true do
-    local found = false -- Флаг для отладки, показывает, был ли найден игрок в этой итерации
-
-    for i = 1, 9 do
+while true do 
+    local found = true
+    for i 1, 9 do
         local lobbyName = "_lobbytemplategreen" .. i
-        local lobby = lobbies:FindFirstChild(lobbyName)
-
-        if lobby then
-            local Players = lobby:FindFirstChild("Players")
-
-            if Players then
-                local valueObject = Players:FindFirstChild("Value")
-
-                if valueObject and valueObject:IsA("ObjectValue") and valueObject.Value and valueObject.Value:IsA("Player") then
-                    local playerName = valueObject.Value.Name
-                    local isMaria = string.find(playerName, "MariaRobinson9") ~= nil
-
-                    if isMaria then
-                        local args = { [1] = lobbyName }
-                        request_join_lobby:InvokeServer(unpack(args))
-                        print("MariaRobinson9 найдена в лобби " .. lobbyName .. ". Запрос на присоединение отправлен.")
-                        found = true -- Игрок найден в этом цикле
-                    end
-                end
+        local value = lobbies:FindFirstChild(lobbyName):FindFirstChild("Players"):FindFirstChild("Value")
+        if value and value.Value and value.Value:IsA("Player") then
+            local playername = value.Value.Name
+            local findname = string.find(playername, user) ~= nil
+            if findname then
+                local args = { 
+                    [1] = lobbyName 
+                }
+                game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("request_join_lobby"):InvokeServer(unpack(args))
+                found = true
             end
         end
     end
-
-    if not found then -- Если игрок не был найден ни в одном лобби
-        print("MariaRobinson9 не найдена ни в одном лобби. Проверка повторяется...")
+    if not found then
+        print("wait for " .. user .. " join.")
     end
-
-    wait(1) -- Задержка 1 секунда (ОБЯЗАТЕЛЬНО!)
+    task.wait(1)
 end
