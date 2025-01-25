3ae1b38b3c040857397c82e7910f20e6103939bf
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-25 23:19:45 +0300

    Update nousigi.lua

--- nousigi.lua
+++ nousigi.lua
@@ -14,7 +14,7 @@ getgenv().Config = {
 	["Performance"] = {
 		["Delete Map"] = true,
 		["Boost FPS"] = true,
-		["Black Screen"] = true
+		["Black Screen"] = false
 	},
 	["Webhook"] = {
 		["Game Finished"] = true,
