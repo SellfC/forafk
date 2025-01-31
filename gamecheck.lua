500b8a7ffea2ac4265dde9416010a1ab408e4be2
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-31 22:42:50 +0300

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -12,5 +12,6 @@ local games = {
 if games[game.CreatorId] then
     loadstring(game:HttpGet(games[game.CreatorId]))()
 else
-    print(game.CreatorId)
+   script_key="EgziitniATxnrsKFCcZtPUqsgpwXgOFo";
+   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9217a7f5a73261867db220523aeafc88.lua"))()
 end
