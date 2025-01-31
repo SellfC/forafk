e8daa1c406a866677109d1701d235881bd3b90ab
SellfC <110660214+SellfC@users.noreply.github.com>
2025-01-31 18:28:45 +0300

    Update aamaria.lua

--- aamaria.lua
+++ aamaria.lua
@@ -1,6 +1,6 @@
-task.wait(20)
-script_key="gHFepjKmnhMwLlrgaPzPXlXgewWWBZor";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/485daf6c6c96201027b51ae694a508d0.lua"))()
+task.wait()
+script_key="pvPebEybMszLyEHlAxlhkjnkKYMzhxYi";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/buang5516/buanghub/main/PremiumBuangHub.lua"))()
 task.wait( math.random() )
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/betterbuffer.lua"))()
 task.wait( math.random() )
