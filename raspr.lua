1071316c0cf3d859876f139e3c5e9952d5667e08
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-07 19:34:19 +0300

    Update raspr.lua

--- raspr.lua
+++ raspr.lua
@@ -2,7 +2,7 @@ local PlayerId = game:GetService("Players").LocalPlayer.UserId
 
 local id = {
     [255556396] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/aamaria.lua', 
-    [2522133596] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/aamaria.lua',
+    [1] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/aamaria.lua',
     [3778767509] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/aamaria.lua'
 }
 
