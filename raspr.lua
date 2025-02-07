07b0987c7b49e94a7bb132403aba05f3091676b6
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-07 21:49:52 +0300

    Update raspr.lua

--- raspr.lua
+++ raspr.lua
@@ -1,6 +1,8 @@
 local ids = {255556396, 2522133596, 3778767509}
 local url = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/aamaria.lua'
 
+print("raspr.lua loaded")
+
 if table.find(ids, game.Players.LocalPlayer.UserId) then
     loadstring(game:HttpGet(url))()
 end
