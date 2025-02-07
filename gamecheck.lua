28b10f4e758d3a1c5366777fde3030a2d91cee6c
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-07 21:42:24 +0300

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -1,16 +1,12 @@
-if not game:IsLoaded() then
-	game.Loaded:Wait()
-end
-
-task.wait( math.random() )
+repeat task.wait() until game:IsLoaded()
 
 local games = {
-	[10611639] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/raspr.lua', -- AA
-        [4372130] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/bfwa.lua' --  BF
+    [10611639] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/raspr.lua',
+    [4372130] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/bfwa.lua'
 }
 
 if games[game.CreatorId] then
     loadstring(game:HttpGet(games[game.CreatorId]))()
 else
-   print(game.CreatorId)
+    print(game.CreatorId)
 end
