1029b6240b6f456d97cedc0d4ea7f243697127ff
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-09 14:20:44 +0300

    Update bfwa.lua

--- bfwa.lua
+++ bfwa.lua
@@ -1,3 +1,5 @@
-repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
-getgenv().Key = "fbiAXRBVdhFpfraKYeasgrPIxpGGLonB" 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))() 
+script_key = "GVnjEUyrpvmpjjywIGZWrdoRjnWSxZHP"
+getgenv().Team = "Pirates"
+getgenv().FixCrash = true -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
+getgenv().FixCrash2 = true -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
