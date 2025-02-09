6dcee180ae42be58c894ffe67a1b8fcad51ecfc2
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-09 17:40:01 +0300

    Update playercountcheck.lua

--- playercountcheck.lua
+++ playercountcheck.lua
@@ -6,6 +6,5 @@ while true do
     if count <= 3 then
         game:GetService("ReplicatedStorage"):WaitForChild("endpoints"):WaitForChild("client_to_server"):WaitForChild("teleport_back_to_lobby"):InvokeServer()
     end
-    print("recheck in 30 sec")
-    task.wait(30)
+    task.wait(60)
 end
