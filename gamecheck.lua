6043f14e928c19c912f0df28b06cc10d758d23eb
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-10 20:23:12 +0300

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -4,7 +4,8 @@ print("game check loaded")
 
 local games = {
     [10611639] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/raspr.lua',
-    [4372130] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/bfwa.lua'
+    [4372130] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/kaitun-bf.lua',
+    [3959677] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/autorank-ps99free.lua'
 }
 
 if games[game.CreatorId] then
