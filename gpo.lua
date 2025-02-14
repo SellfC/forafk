3deadaba021cd568fa999a4dad3ad02a52dd7929
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-14 22:45:23 +0300

    Create gpo.lua

---
+++ gpo.lua
@@ -0,0 +1,9 @@
+getgenv().BuyStaminaPotions = true
+getgenv().BuyMythicalChest = true
+getgenv().BuyLegendaryChest = true
+getgenv().StopOnShop = false
+getgenv().MythicalCostCap = 375453
+getgenv().code = "ODmcKl4ZQl"
+getgenv().WebHook = "https://discord.com/api/webhooks/1292222634272227399/u4Ml8C-UzLO08a1osvu9S9z7fbwKkW1xq7CNPnfsADmLmVcJlHleEKvJvBQ3wW2H6Tlt"
+script_key="EgziitniATxnrsKFCcZtPUqsgpwXgOFo";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/98b3f54aeb618d45b53b134a25a41685.lua"))()
