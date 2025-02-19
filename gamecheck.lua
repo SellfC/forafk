ca34ff1b3596c3f79a31d7b7cae5d9774c64cc31
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-19 17:45:18 +0300

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -4,7 +4,7 @@ print("game check loaded")
 
 local games = {
     [10611639] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/raspr.lua',
-    [4372130] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/bfwa.lua',
+    [4372130] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/kaitun-bf.lua',
     [3959677] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/autorank-ps99free.lua',
     [12229756] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/raspr.lua'
 }
