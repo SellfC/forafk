fdfc8726f0da81eba4954d99f334cb504c631568
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-25 21:34:35 +0300

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -6,7 +6,8 @@ local games = {
     [10611639] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/raspr.lua',
     [4372130] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/bfwa.lua',
     [3959677] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/autorank-ps99free.lua',
-    [12229756] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/raspr.lua'
+    [12229756] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/raspr.lua',
+    [17219742] = 'https://raw.githubusercontent.com/SellfC/forafk/refs/heads/main/nousigi.lua'
 }
 
 if games[game.CreatorId] then
