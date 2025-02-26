31c344d95ead68e0ef444b763a9f7a44b9621c7b
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-26 22:52:44 +0300

    Update bfwa.lua

--- bfwa.lua
+++ bfwa.lua
@@ -44,3 +44,4 @@ getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance
 getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
 end
+end
