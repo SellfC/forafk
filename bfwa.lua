c49668b1ef5cafe70dc110909bf37cd4e1b3167b
SellfC <110660214+SellfC@users.noreply.github.com>
2025-02-26 22:50:37 +0300

    Update bfwa.lua

--- bfwa.lua
+++ bfwa.lua
@@ -1,4 +1,40 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+if getgenv().findfruit == true then
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Key = "c05a75937114c5cb37e7fa03"
+getgenv().Setting = {
+    ["Delay Hop"] = 0,
+    ["Webhook"] = {
+        ["url"] = "https://discord.com/api/webhooks/1290727516256075827/RYR2_LHEmk-5fbii_cOuq_hfZXCWw5w1SQ7lEw1DbomFS91xjq2F5-ApzFE84h1U_961",
+        ["Webhook Target Fruit"] = true,
+        ["Webhook Store Fruit"] = {
+            ["Rarity"] = {
+                ["Mythical"] = true,
+                ["Legendary"] = false, 
+                ["Rare"] = false,
+                ["Uncommon"] = false,
+                ["Common"] = false,
+            },
+            ["Enabled"] = true, 
+        },
+        ["Webhook When Attack Factory"] = true,
+        ["Webhook When Attack Raid Castle"] = true,
+        ["Ping Discord"] = {
+            ["Enabled"] = true, 
+            ["Id Discord/Everyone"] = "952168231827812412"
+        },
+        ["Enabled"] = true,
+    },
+    ["Auto Random Fruit"] = true,
+    ["Use Portal Teleport"] = true,
+    ["Attacking"] = {
+        ["Weapon"] = "Melee", -- Sword,Melee,Blox Fruit
+        ["Raid Castle"] = true,
+        ["Factory"] = true,
+    }
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/refs/heads/main/HopFruit.lua"))()
+else
 if getgenv().banana == true then
 getgenv().Key = "c05a75937114c5cb37e7fa03" 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))() 
