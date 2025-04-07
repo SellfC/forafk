54a481f82490bd3e115ecb5a0f1eda95d74395f9
SellfC <110660214+SellfC@users.noreply.github.com>
2025-04-07 23:16:35 +0300

    Update nousigi.lua

--- nousigi.lua
+++ nousigi.lua
@@ -1,2 +1,15 @@
-getgenv().Key = "k8f732e0f88c9d260d9ae406"
-repeat wait()spawn(function()loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()end)wait(20)until Joebiden
+if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
+getgenv().Script_Mode = "Kaitun_Script"
+_G.VanguardSettings = {
+    ['ClaimDaily'] = true,
+    ['ClaimQuests'] = true,
+    ['ClaimAchievement'] = true,
+    ['ClaimBattlepass'] = true,
+    ['Webhooks'] = {
+        ['Enabled'] = true,
+        ['Url'] = ''
+    }
+}
+(getgenv()).key = "MARUG5M1H8Y4Y9GLMXANGUMSM3";
+(getgenv()).id = "952168231827812412";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
