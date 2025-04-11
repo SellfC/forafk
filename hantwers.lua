6631089f186e7071e5a80ef4e6bd750cefa6b84e
SellfC <110660214+SellfC@users.noreply.github.com>
2025-04-11 23:09:26 +0300

    Update hantwers.lua

--- hantwers.lua
+++ hantwers.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OhhMyGehlee/ter/refs/heads/main/hint"))()
+(getgenv()).key = "MARUG5M1H8Y4Y9GLMXANGUMSM3";
+(getgenv()).id = "952168231827812412";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
