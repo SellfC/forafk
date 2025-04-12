2ba1275f73f3911188f4d2300f4e1749a3b7b189
SellfC <110660214+SellfC@users.noreply.github.com>
2025-04-12 14:29:39 +0300

    Update hantwers.lua

--- hantwers.lua
+++ hantwers.lua
@@ -1,3 +1,2 @@
-(getgenv()).key = "MARUG5M1H8Y4Y9GLMXANGUMSM3";
-(getgenv()).id = "952168231827812412";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
+script_key="wJdrSJOIIaPrhFdpCapwJiJWsIMXwGKV";
+loadstring(game:HttpGet("https://scripts.alchemyhub.xyz"))()
