16abe1876d9b11aeb5cefc4efa3d921d60a37544
SellfC <110660214+SellfC@users.noreply.github.com>
2025-04-18 06:48:28 +0300

    Update hantwers.lua

--- hantwers.lua
+++ hantwers.lua
@@ -1,2 +1,3 @@
-script_key="wJdrSJOIIaPrhFdpCapwJiJWsIMXwGKV";
-loadstring(game:HttpGet("https://scripts.alchemyhub.xyz"))()
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
+getgenv().Key = "c05a75937114c5cb37e7fa03" 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
