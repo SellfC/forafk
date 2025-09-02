4b5fb98ca4f310cbd557668eb582cb9c86e298b3
SellfC <110660214+SellfC@users.noreply.github.com>
2025-09-02 08:55:01 +0300

    Create lure.lua

---
+++ lure.lua
@@ -0,0 +1,5 @@
+getgenv().PsCode = "VezjhDNEvi"
+getgenv().shipspawnkillers = 2 --how many killers u want to be in position for ships to spawn
+
+script_key="OnceJnwOrwgSJEGIdnbRMlOtlmcCSDSa";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8d6a4b2631117d5e7ca857ae8738b0c8.lua"))()
