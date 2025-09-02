578e314e6080dd4b4b1c2be39d1600e635a31a9a
SellfC <110660214+SellfC@users.noreply.github.com>
2025-09-02 08:58:12 +0300

    Update lure.lua

--- lure.lua
+++ lure.lua
@@ -3,3 +3,7 @@ getgenv().shipspawnkillers = 2 --how many killers u want to be in position for s
 
 script_key="OnceJnwOrwgSJEGIdnbRMlOtlmcCSDSa";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/8d6a4b2631117d5e7ca857ae8738b0c8.lua"))()
+while true do
+wait(0.1)
+setfpscap(15)
+end
