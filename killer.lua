5fc3f1fa72f58b54af36b40b91a20db596a0f2f2
SellfC <110660214+SellfC@users.noreply.github.com>
2025-09-13 14:49:12 +0300

    Update killer.lua

--- killer.lua
+++ killer.lua
@@ -1,6 +1,20 @@
-getgenv().PsCode = "VezjhDNEvi"
-getgenv().Webhook = "https://discord.com/api/webhooks/1411704389550211123/QpQVvJOpLiDc_UOBroLhlW_xB7cA1lR8MhUJ5epNz9WSa6shwESQxqWQydIeL7xluTvx"
-getgenv().AutoDropRare = true --deletes rare fruits if true
+getgenv().AutoKrakenFarm = true
+getgenv().KrakenType = "GKK" -- or KK
+getgenv().AutoAccRole = true -- Auto Chooses Lure/Killer/Anchor
 
-script_key="OnceJnwOrwgSJEGIdnbRMlOtlmcCSDSa";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/1688763f40874b920132070abf865c66.lua"))()
+getgenv().AutoUseHaki = false -- Uses Buso Haki
+getgenv().AutoDeleteRareFruits = true -- deletes Rare fruits
+
+getgenv().PsCode = "VezjhDNEvi" -- Private Server Code
+getgenv().AutoKick = 5 -- Auto Kicks in 10min Only works on Lures Good For anti Stucking
+getgenv().Whitescreen = true -- Cpu Saver true/false
+getgenv().FastMode = true -- Lowers Grapics
+getgenv().Webhook = "https://discord.com/api/webhooks/1413075592256946236/oDme30_Wahs2IDveHqg4EVebcD2IVoj4viE3sIwUJpcLRnwggVdtRXIsmEVt4el0ZfB_"
+getgenv().LoadGuiConfig = false -- Doesnt Load your in game ui config
+script_key = "OnceJnwOrwgSJEGIdnbRMlOtlmcCSDSa";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Esaiemos/Zeph/refs/heads/main/ZephScript"))()
+
+while true do
+task.wait(10)
+setfpscap(20)
+end
