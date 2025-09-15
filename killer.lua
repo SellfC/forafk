bd77eee733ece6c1b88d38cb8eb3bbf8cf9ed219
SellfC <110660214+SellfC@users.noreply.github.com>
2025-09-16 00:47:42 +0300

    Update killer.lua

--- killer.lua
+++ killer.lua
@@ -7,13 +7,12 @@ getgenv().AutoDeleteRareFruits = true -- deletes Rare fruits
 
 getgenv().PsCode = "VezjhDNEvi" -- Private Server Code
 getgenv().AutoKick = 5 -- Auto Kicks in 10min Only works on Lures Good For anti Stucking
-getgenv().Whitescreen = true -- Cpu Saver true/false
+getgenv().Whitescreen = false -- Cpu Saver true/false
 getgenv().FastMode = true -- Lowers Grapics
 getgenv().Webhook = "https://discord.com/api/webhooks/1413075592256946236/oDme30_Wahs2IDveHqg4EVebcD2IVoj4viE3sIwUJpcLRnwggVdtRXIsmEVt4el0ZfB_"
 getgenv().LoadGuiConfig = false -- Doesnt Load your in game ui config
 script_key = "OnceJnwOrwgSJEGIdnbRMlOtlmcCSDSa";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Esaiemos/Zeph/refs/heads/main/ZephScript"))()
-
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b5a22900b1844b471fb009be4a2f8fb.lua"))()
 while true do
 task.wait(10)
 setfpscap(20)
