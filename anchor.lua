7ed318294717bd1f1ab115360644156f87e16649
SellfC <110660214+SellfC@users.noreply.github.com>
2025-09-16 00:47:24 +0300

    Update anchor.lua

--- anchor.lua
+++ anchor.lua
@@ -12,7 +12,7 @@ getgenv().FastMode = true -- Lowers Grapics
 getgenv().Webhook = "https://discord.com/api/webhooks/1413075592256946236/oDme30_Wahs2IDveHqg4EVebcD2IVoj4viE3sIwUJpcLRnwggVdtRXIsmEVt4el0ZfB_"
 getgenv().LoadGuiConfig = false -- Doesnt Load your in game ui config
 script_key = "OnceJnwOrwgSJEGIdnbRMlOtlmcCSDSa";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Esaiemos/Zeph/refs/heads/main/ZephScript"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b5a22900b1844b471fb009be4a2f8fb.lua"))()
 
 while true do
 task.wait(10)
