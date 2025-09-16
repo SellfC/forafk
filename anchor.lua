5870862b280db02e4675a129687414b1070a0e23
SellfC <110660214+SellfC@users.noreply.github.com>
2025-09-16 23:13:52 +0300

    Update anchor.lua

--- anchor.lua
+++ anchor.lua
@@ -15,6 +15,6 @@ script_key = "OnceJnwOrwgSJEGIdnbRMlOtlmcCSDSa";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2b5a22900b1844b471fb009be4a2f8fb.lua"))()
 
 while true do
-task.wait(10)
+task.wait(0.1)
 setfpscap(7)
 end
